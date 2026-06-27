41aeed4eb4293cab18d41fd3ca6878e4a43eaf6a
NirmalMishra08 <145590866+NirmalMishra08@users.noreply.github.com>
2026-06-28 00:03:37 +0530

    initial schema completed

--- backend/db/schema/schema.sql
+++ backend/db/schema/schema.sql
@@ -13,43 +13,57 @@ CREATE type ride_request_status as ENUM('SEARCHING', 'MATCHED','CANCELLED','COMP
 
 CREATE TYPE trip_status as ENUM ('COMPLETED', 'ONGOING', 'REJECTED');
 
+CREATE type vehicle_type as enum('SEDAN', 'SUV');
+
 CREATE table users (
-    id UUID PRIMARY KEY gen_random__uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name text not NULL,
+    role user_role NOT NULL,
     email text not NULL,
-    phone INT,
+    phone VARCHAR(15),
     provider provider,
-    created_at TIMESTAMPZ
+    created_at TIMESTAMPTZ DEFAULT now()
 );
 
-CREATE Table if not exists riders (
+CREATE Table rider_profile (
    user_id UUID REFERENCES users(id),
-   name TEXT,
-   phone BIGINT
-
+   rating DOUBLE precision
 );
 
-
-
-CREATE Table if not exists drivers (
+CREATE Table if not exists driver_profile (
     user_id UUID REFERENCES users(id),
-    vehicle_id text,
     license_number INT,
     status driver_status,
-    latitude TEXT ,
-    longitude TEXT,
-    updated_at timestampz
+    rating DOUBLE precision,
+    updated_at timestamptz
 );
+
+
+CREATE Table if not exists vehicles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    driver_id UUID REFERENCES driver_profile(user_id),
+    model TEXT,
+    plate_number TEXT,
+    type vehicle_type
+);
+
+CREATE Table driver_location (
+    driver_id UUID REFERENCES driver_profile(user_id),
+    latitude DOUBLE precision,
+    longitude DOUBLE precision,
+);
+
+
 
 CREATE Table ride_requests(
     id UUID PRIMARY key DEFAULT gen_random_uuid(),
-    rider_id UUID REFERENCES riders(id),
-    pickup_lat TEXT,
-    pickup_long TEXT,
-    destination_lat TEXT,
-    destination_lat TEXT,
+    rider_id UUID REFERENCES rider_profile(user_id),
+    pickup_lat DOUBLE precision,
+    pickup_long DOUBLE precision,
+    destination_lat DOUBLE precision,
+    destination_long DOUBLE precision,
     status ride_request_status,
-    created_at TIMESTAMPZ
+    created_at TIMESTAMPtZ
 );
 
 CREATE Table trips (
@@ -57,7 +71,7 @@ CREATE Table trips (
     ride_request_id UUID REFERENCES ride_request(id),
     driver_id UUID REFERENCES  drivers(id),
     status trips_status,
-    started_at TIMESTAMPZ DEFAULT now(),
-    completed_at TIMESTAMPZ
+    started_at TIMESTAMPtZ DEFAULT now(),
+    completed_at TIMESTAMPtZ
 );
 
